86183c02280c1a1db8010452e5f19ebff7855397
alaz <akaleli1@binghamton.edu>
2021-11-18 18:05:35 -0500

    project restarted.

---
+++ CrossBirds-Alaz/CrossBirds-Alaz/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  CrossBirds-Alaz
+//
+//  Created by alaz kalelioglu on 18.11.2021.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+        
+}
+
