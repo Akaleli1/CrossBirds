a6ec1ff4989ed1d9db34027838717ca7f30d67b0
alaz <akaleli1@binghamton.edu>
2021-11-20 18:10:55 -0500

    Bird class features initialized.

--- CrossBirds-Alaz/CrossBirds-Alaz/myBird.swift
+++ CrossBirds-Alaz/CrossBirds-Alaz/myBird.swift
@@ -7,6 +7,35 @@
 
 import SpriteKit
 
-class myBird: SKSpriteNode {
+enum myBirdType: String{
+    
+    case red, blue, yellow, gray
+}
 
+class myBird: SKSpriteNode {
+    
+    let birdType: myBirdType
+    
+    init(type: myBirdType){
+        birdType = type
+        
+        let color: UIColor!
+        switch type{          //My bird colors
+        case .red:
+            color = UIColor.red
+        case .blue:
+            color = UIColor.blue
+        case .yellow:
+            color = UIColor.yellow
+        case .gray:
+            color = UIColor.gray
+        }
+        
+        super.init(texture: nil, color: color, size: CGSize(width: 40.0, height:40.0))
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {  //this is something XCode suggested to get rid of an error
+        fatalError("init(coder:) has not been implemented")
+    }
 }
